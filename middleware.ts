ab468637180adb29786226e53c9f41fb578d30f4
omarlari <omarlari@gmail.com>
2022-08-25 14:08:30 -0700

    middelware addition

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+// middleware.ts
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = 'SE';
+
+// Trigger this middleware to run on the `/secret-page` route
+export const config = {
+  matcher: '/omarlari',
+};
+
+export function middleware(req: NextRequest) {
+  // Extract country. Default to US if not found.
+  const country = (req.geo && req.geo.country) || 'US';
+
+  console.log(`Visitor from ${country}`);
+
+  // Specify the correct route based on the requests location
+  if (country === BLOCKED_COUNTRY) {
+    req.nextUrl.pathname = '../pages/login';
+  } else {
+    req.nextUrl.pathname = `../pages/index.js`;
+  }
+
+  // Rewrite to URL
+  return NextResponse.rewrite(req.nextUrl);
+}
