88d452f7ae3565cfd0decffbe3d7d2e2ab702d81
omarlari <omarlari@gmail.com>
2022-08-25 13:11:43 -0700

    intro

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I am Omar. I am a product manager and passionate about Developer Experiences.</p>
+        <p>Hello, I am Omar. I am a product manager and am passionate about creating amazing Developer Experiences.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
